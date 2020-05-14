cd70bf68089bba4937bd396ecb4aaa1e26cf69dd
Jonas Smith Müller <jonassmithmuller@gmail.com>
2020-05-14 23:56:43 +0200

    Added package

--- src/Lira/Backends/Solidity/SolCompiler.hs
+++ src/Lira/Backends/Solidity/SolCompiler.hs
@@ -3,30 +3,29 @@ module Lira.Backends.Solidity.SolCompiler where
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 
+import NeatInterpolation
 import Data.Text (Text)
-import Text.RawString.QQ (r)
-import NeatInterpolation (text)
 
-assemble :: IntermediateContract -> Text
+assemble :: Text
 assemble = contract
 
 head :: Text
-head = [r|test1|]
+head = [trimming|test1|]
 
 activate :: Text
-activate = [r|test2|]
+activate = [trimming|test2|]
 
 execute :: Text
-execute = [r|test3|]
+execute = [trimming|test3|]
 
 contract :: Text
-contract = [r|pragma solidity ^0.6.4;
+contract = [trimming|pragma solidity ^0.6.4;
 
 contract Contract1 {
-  ${head}
+  $head
 
-  ${activate}
+  $activate
 
-  ${execute}
+  $execute
   }
 }|]
