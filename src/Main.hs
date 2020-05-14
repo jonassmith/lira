f2be05d632bdd6345dbdf1eb8b1f210d06cf1b03
Jonas Smith Müller <jonassmithmuller@gmail.com>
2020-05-14 16:59:27 +0200

    Added Solidity backend

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
 -- MIT License
--- 
+--
 -- Copyright (c) 2019 eToroX Labs
--- 
+--
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the "Software"), to deal
 -- in the Software without restriction, including without limitation the rights
 -- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 -- copies of the Software, and to permit persons to whom the Software is
 -- furnished to do so, subject to the following conditions:
--- 
+--
 -- The above copyright notice and this permission notice shall be included in all
 -- copies or substantial portions of the Software.
--- 
+--
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 -- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 -- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,12 +37,14 @@ import           System.IO (stderr)
 
 import           Lira.Version (showVersion, version)
 import qualified Lira.Backends.Evm as Evm
+import qualified Lira.Backends.Solidity as Sol
 
 type OutputFile = (FilePath, Text)
 type Backend = FilePath -> Text -> Either Text [OutputFile]
 
 backends :: [(Text, Backend)]
-backends = [ ("evm", Evm.compile) ]
+backends = [ ("evm", Evm.compile),
+            ("sol", Sol.compile) ]
 
 data Args = Args
   { srcFile :: FilePath -- ^ The Lira contract file
