0c9bd8edf44142127c4e3adebcc6bf7e70697cf0
Jonas Smith Müller <jonassmithmuller@gmail.com>
2020-05-15 14:42:19 +0200

    Now pretty printing expressions

--- src/Lira/Backends/Solidity/SolCompiler.hs
+++ src/Lira/Backends/Solidity/SolCompiler.hs
@@ -9,6 +9,9 @@ import           Control.Monad.Reader
 import           Lira.Contract.Intermediate
 import           Lira.TypeChecker
 import           Control.Monad
+import           Lira.Contract
+import qualified Data.Text as Text
+import           Data.Text.Conversions
 
 assemble :: IntermediateContract -> Text
 assemble intermediateContract = contract intermediateContract
@@ -21,14 +24,48 @@ activate ic = transferCalls' (getTransferCalls ic)
   where
     transferCalls' :: [TransferCall] -> Text
     transferCalls' text = ""
-    transferCalls' (tc:tcs) = [text|common_token.transferFrom(${_from tc}, address(this), ${_amount tc})\n|] <> transferCalls' tcs
+    transferCalls' (tc:tcs) = [text|common_token.transferFrom(${_from tc}, address(this), ${_maxAmount tc})\n|] <> transferCalls' tcs
 
 execute :: IntermediateContract -> Text
 execute ic = transferCalls' (getTransferCalls ic)
   where
     transferCalls' :: [TransferCall] -> Text
     transferCalls' text = ""
-    transferCalls' (tc:tcs) = [text|common_token.transfer(${_to tc}, ${_amount tc})\n|] <> transferCalls' tcs
+    transferCalls' (tc:tcs) = [text|common_token.transfer(${_to tc}, ${ppSolExpr (_amount tc)})\n|] <> transferCalls' tcs
+
+ppSolExpr :: Expr -> Text
+ppSolExpr (Lit e1) = ppSolLiteral e1
+ppSolExpr (MinExp e1 e2) =
+  let s1 = ppSolExpr e1
+      s2 = ppSolExpr e2
+  in Text.concat [ "((", s1, " < ", s2, ") ? (", s1, ") : (", s2, "))" ]
+ppSolExpr (MaxExp e1 e2) =
+  let s1 = ppSolExpr e1
+      s2 = ppSolExpr e2
+  in Text.concat [ "((", s1, " > ", s2, ") ? (", s1, ") : (", s2, "))" ]
+ppSolExpr (MultExp e1 e2) = "(" <> ppSolExpr e1 <> " * " <> ppSolExpr e2 <> ")"
+ppSolExpr (DiviExp e1 e2) = "(" <> ppSolExpr e1 <> " / " <> ppSolExpr e2 <> ")"
+ppSolExpr (AddiExp e1 e2) = "(" <> ppSolExpr e1 <> " + " <> ppSolExpr e2 <> ")"
+ppSolExpr (SubtExp e1 e2) = "(" <> ppSolExpr e1 <> " - " <> ppSolExpr e2 <> ")"
+ppSolExpr (LtExp e1 e2) = "(" <> ppSolExpr e1 <> " > " <> ppSolExpr e2 <> ")"
+ppSolExpr (GtExp e1 e2) = "(" <> ppSolExpr e1 <> " < " <> ppSolExpr e2 <> ")"
+ppSolExpr (EqExp e1 e2) = "(" <> ppSolExpr e1 <> " == " <> ppSolExpr e2 <> ")"
+ppSolExpr (GtOrEqExp e1 e2) = "(" <> ppSolExpr e1 <> " <= " <> ppSolExpr e2 <> ")"
+ppSolExpr (LtOrEqExp e1 e2) = "(" <> ppSolExpr e1 <> " >= " <> ppSolExpr e2 <> ")"
+ppSolExpr (NotExp e1) = "(!" <> ppSolExpr e1 <> ")"
+ppSolExpr (AndExp e1 e2) = "(" <> ppSolExpr e1 <> " && " <> ppSolExpr e2 <> ")"
+ppSolExpr (OrExp e1 e2) = "(" <> ppSolExpr e1 <> " || " <> ppSolExpr e2 <> ")"
+ppSolExpr (IfExp e1 e2 e3) = "(if(" <> ppSolExpr e1 <> "){" <> ppSolExpr e2 <> "} else {" <> ppSolExpr e3 <> "})"
+
+ppSolLiteral :: Literal -> Text
+ppSolLiteral (IntVal e1) = convertText (show e1)
+ppSolLiteral (BoolVal e1) = boolToString e1
+-- TODO: Observerables
+-- ppSolLiteral (e1 e2 e3 e4 e5) =
+
+boolToString :: Bool -> Text
+boolToString True = "True"
+boolToString False = "False"
 
 contract :: IntermediateContract -> Text
 contract ic = [text|pragma solidity ^0.6.4;
